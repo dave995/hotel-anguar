bfac99e796b036de20bb7c131aac48883f3b63bf
Daouda Thiam <daoudathiam95@gmail.com>
2023-02-01 17:25:47 +0000

    modifier les nom dhotel

--- src/app/hotels/shared/api/hotel.data.ts
+++ src/app/hotels/shared/api/hotel.data.ts
@@ -15,33 +15,33 @@ export class HotelData implements InMemoryDbService {
     const hotels: IHotel[] = [
       {
         id: 1,
-        hotelName: 'Buea sweet life',
+        hotelName: 'Radison Hotel',
         description: 'Belle vue au bord de la mer',
-        price: 230.5,
+        price:  200000,
         imageUrl: 'assets/img/hotel-room.jpg',
-        rating: 3.5,
+        rating: 4.5,
         tags: ['nouveau']
       }, {
         id: 2,
-        hotelName: 'Marakech',
+        hotelName: 'King Fahd Place',
         description: 'Profitez de la vue sur les montagnes',
-        price: 145.5,
+        price: 145500,
         imageUrl: 'assets/img/the-interior.jpg',
         rating: 5,
         tags: ['nouveau']
       }, {
         id: 3,
-        hotelName: 'Abudja new look palace',
+        hotelName: 'Teroubi',
         description: 'Séjour complet avec service de voitures',
-        price: 120.12,
+        price: 120120,
         imageUrl: 'assets/img/indoors.jpg',
         rating: 4,
         tags: ['nouveau']
       }, {
         id: 4,
-        hotelName: 'Cape town city',
+        hotelName: 'Saly Hotel',
         description: 'Magnifique cadre pour votre séjour',
-        price: 135.12,
+        price: 135120,
         imageUrl: 'assets/img/window.jpg',
         rating: 2.5,
         tags: ['nouveau']
